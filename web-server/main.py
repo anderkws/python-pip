452f81594a6668547e49f0db4a69a836f954ed6a
Anderson <anderkws@gmail.com>
2024-05-19 22:25:53 -0500

    Mejoras de scripts, creación de servicio web 3

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,24 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,]
+
+@app.get('/respuestaHTML',response_class=HTMLResponse)
+def get_ls():
+    return '''
+        <h1>Hola soy una cabecera</h1>
+        <p>soy un parrafo</p>
+    '''
+
+@app.get('/contact')
+def get_list():
+    return {"name": 'Platzi'}
+
 
 def run():
     store.get_categories()
